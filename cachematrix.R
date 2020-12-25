56097b855f9961a963a9bdc45e26b125a4bf4319
PRATEEKNIS <75770426+PRATEEKNIS@users.noreply.github.com>
2020-12-25 14:45:29 +0530

    Add files via upload
    Please read the coding, which is right as mark accordingly

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im<-NULL
+  set<-function(y){
+    x<<-y
+    im<<-NULL
+  }
+  get<-function() x
+  setmat<-function(matx) im<<-matx
+  getmat<function() im
+  list(set=set,get=get,setmat=setmat,getmat=getmat)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  f<-x$getmat()
+  if(!is.null(f)){
+    message("getting inverse catched matrix")
+    return(f)
+  }
+  data<-x$get()
+  g<-solve(data, ...)
+  x$setmat(g)
+  g
 }
